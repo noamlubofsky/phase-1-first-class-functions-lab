3a2bc134bf2fdf5f55256193dfe521829bc746b4
github username <github email address>
2021-06-12 21:48:20 -0400

    commit tree ad9adb34723906ac19c3cbdba0d65bc073cd6a84

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2)
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2, 4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fare) {
+    function fareQuintupler() {return (fare * 5)}
+    return fareQuintupler
+}
+
+function fareDoubler(fare) {
+    const doubler = (fare * 2)
+    return doubler
+}
+
+function fareTripler(fare) {
+    const tripler = (fare * 3)
+    return tripler
+}
+
+function selectDifferentDrivers(drivers, func) {
+    return (func(drivers))
+}
